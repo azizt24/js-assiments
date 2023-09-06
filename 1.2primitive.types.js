573e49b9b11d431cbf14ba683444bd8e9a0fe674
azizt24 <78258864+azizt24@users.noreply.github.com>
2023-09-06 13:25:33 +0300

    push

---
+++ 1.2primitive.types.js
@@ -0,0 +1,74 @@
+/*--Delete the wrong answers--
+1. Which of the following is/are strings?
+
+c) '4'
+2. Which of the following is/are numbers?
+a) 4
+b) 4.0
+
+d) -4
+3. Which of the following is/are booleans?
+a) true
+b) false
+
+4. What is the result of 80 + 71.2?
+a) 151.2
+
+5. What is the result of "80" + 71.2?
+
+c) "8071.2"
+6. Does 100 + 30 produce a number or a string?
+a) number
+ 
+7. Does "100" + 30 produce a number or a string?
+ 
+b)string
+*/
+//------Submit your answers under the question//
+
+//create three different ways to declare variables
+var name = "aziz"
+var age = 26
+var aziz = ('is a girl') = false
+
+let name = "aziz"
+let age = 26
+let aziz = ('is a girl') = false
+
+const name = "aziz"
+const age = 26
+const aziz = ('is a girl') = false
+//declare a variable and reassign a value to it
+
+let aziz = 26 
+
+aziz = 55
+//create a variable and after that assign a string with its value being: He's got it!
+let newValue = "He's got it!"
+
+/*
+1. create a variable and assign a value on how
+much a restaurant bill is.
+2. create a variable and assign a value on how
+much tax they should pay.
+3. create a variable that will calculate the bill
+* tax and its output would be: Your total bill is
+<total bill> $.
+*/
+let restaurantBill = 250 
+
+let taxBill = 30
+
+totalBill = restaurantBill + taxBill
+
+console.log =" tatal bill ".totalBill;
+
+
+// Round the number 50.6 to its nearest integer
+
+let roundedNumber = Math.round(50.6);
+//Create a variable that is undefined
+
+let undefinedVariable;
+
+let undefinedVariable = undefined;
